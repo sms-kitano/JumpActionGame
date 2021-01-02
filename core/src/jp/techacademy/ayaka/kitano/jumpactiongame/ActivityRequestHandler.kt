5d6ca13c3ee8023fbc32df0488c6a94db0d76989
sms-kitano <ayaka-kitano@bm-sms.co.jp>
2021-01-02 14:52:53 +0900

    Initial commit

---
+++ core/src/jp/techacademy/ayaka/kitano/jumpactiongame/ActivityRequestHandler.kt
@@ -0,0 +1,5 @@
+package jp.techacademy.ayaka.kitano.jumpactiongame
+
+interface ActivityRequestHandler {
+    fun showAds(show: Boolean)
+}
---
+++ core/src/jp/techacademy/ayaka/kitano/jumpactiongame/Star.kt
@@ -0,0 +1,31 @@
+package jp.techacademy.ayaka.kitano.jumpactiongame
+
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.Sprite
+
+class Star(texture: Texture, srcX: Int, srcY: Int, srcWidth: Int, srcHeight: Int)
+    : Sprite(texture, srcX, srcY, srcWidth, srcHeight) {
+
+    companion object {
+        // 横幅、高さ
+        val STAR_WIDTH = 0.8f
+        val STAR_HEIGHT = 0.8f
+
+        // 状態
+        val STAR_EXIST = 0
+        val STAR_NONE = 1
+    }
+
+    var mState: Int = 0
+
+    init {
+        setSize(STAR_WIDTH, STAR_HEIGHT)
+        mState = STAR_EXIST
+    }
+
+    //プレイヤーが触れた時に呼ばれる
+    fun get() {
+        mState = STAR_NONE
+        setAlpha(0f)
+    }
+}
